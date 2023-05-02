ee0ee30a0adcc043405d50766deed95753dc3c9e
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-05-02 09:42:47 +0530

    okkkkkkk

--- route/user.route.js
+++ route/user.route.js
@@ -2,7 +2,6 @@ const express=require('express')
 const { UserModel } = require('../model/user.model')
 const router=express.Router()
 var jwt = require('jsonwebtoken');
-const { auth } = require('../middleware/auth.middleware');
 const bcrypt = require('bcrypt');
 router.get('/',(req,res)=>{
     res.status(200).send({"msg":"welcome to user page"})
@@ -11,13 +10,10 @@ router.post('/register',async(req,res)=>{
     const {username,password}=req.body
     try {
         bcrypt.hash (password, 5, async(err, hash)=>{
-            if(err){
-                 res.status(500).send({"msg":"register failed"})
-            }
             const user=new UserModel({username,password:hash})
             await user.save()
+            res.status(200).send({"msg":"New User Added"})       
         });
-        res.status(200).send({"msg":"New User Added"})       
     } catch (error) {
         res.status(400).send({"msg":error.msg})
     }
@@ -42,9 +38,5 @@ router.post('/login',async(req,res)=>{
     } catch (error) {
         res.status(400).send({"msg":error.msg})
     }
-})
-
-router.get("/posts",auth,(req,res)=>{
-    res.status(200).send({"msg":"posts data"})
 })
 module.exports={router}
