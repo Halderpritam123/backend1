78facb3bcff5f986546594a87d289e9c7ae48ae7
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-05-02 08:26:23 +0530

    vhjvf

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ const { auth } = require('./middleware/auth.middleware')
 const cors=require('cors')
 require("dotenv").config()
 const app=express()
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-    next();
-  });
-// app.use(cors())
+
+app.use(cors())
 app.use(express.json())
 app.use("/users",router)
 
